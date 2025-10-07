688dd54751a3e75b6709959f67034f46ad45b488
maxi <max.sandejas@gmail.com>
2025-10-07 18:24:34 +0800

    need to fix object destroy

---
+++ Assets/ObjectDestroy.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+{
+    public GameObject Enemy;
+
+
+}
--- Assets/TowerManager.cs
+++ Assets/TowerManager.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class TowerManager : MonoBehaviour
 {
     public float radius;
-    
+  
     private void EnemyDetector(Vector3 center, float radius)
     {
         Collider[] hitColliders = Physics.OverlapSphere(center, radius);
         foreach (var hitCollider in hitColliders)
         {
-            hitCollider.SendMessage("Enemy Incoming");
+            hitCollider.SendMessage("Attack Enemy");
         }
     }
 
