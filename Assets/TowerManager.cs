98e63fb596f89fefd0c09512b8ed25923697e1d2
maxi <max.sandejas@gmail.com>
2025-10-07 17:37:39 +0800

    hi

---
+++ Assets/TowerManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+    public float radius;
+    
+    private void EnemyDetector(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        foreach (var hitCollider in hitColliders)
+        {
+            hitCollider.SendMessage("Enemy Incoming");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
